6c997afef14c3a6648a26b9b21d9762097a4ba99
UBlandine <124615325+UBlandine@users.noreply.github.com>
2024-08-29 12:19:29 +0200

    Today's update

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import allRoutes from "./routes/index.js";
+import swaggerJsdoc  from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+
+
 dotenv.config();
 
 app.use(express.json());
@@ -21,6 +25,27 @@ app.get('/', (req, res) => {
 
 app.use("/api/v1", allRoutes);
 
+// Swagger setup
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Tickets Management System API",
+      version: "1.0.0",
+      description: "A simple API for managing tickets operations.",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
